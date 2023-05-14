7bd524e166a781b13b95b97942543d6417943a0a
MatiasSalomon01 <matisalomond@gmail.com>
2023-05-13 22:58:30 -0400

    Get_it

--- bases_web/lib/main.dart
+++ bases_web/lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:bases_web/locator.dart';
 import 'package:bases_web/router/route_generator.dart';
 import 'package:bases_web/services/navigation_service.dart';
 import 'package:bases_web/ui/layout/main_layout_page.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MyApp());
+void main() {
+  setupLocator();
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -15,7 +19,7 @@ class MyApp extends StatelessWidget {
       title: 'Rutas App',
       initialRoute: 'stateful',
       onGenerateRoute: RouteGenerator.generateRoute,
-      navigatorKey: navigationService.navigatorKey,
+      navigatorKey: locator<NavigationService>().navigatorKey,
       builder: (context, child) {
         return MainLayoutPage(child: child ?? Container());
       },
--- bases_web/lib/ui/shared/custom_app_menu.dart
+++ bases_web/lib/ui/shared/custom_app_menu.dart
@@ -1,3 +1,4 @@
+import 'package:bases_web/locator.dart';
 import 'package:bases_web/services/navigation_service.dart';
 import 'package:bases_web/ui/shared/custom_flat_button.dart';
 import 'package:flutter/material.dart';
@@ -14,19 +15,21 @@ class CustomAppMenu extends StatelessWidget {
         children: [
           CustomFlatButton(
             text: 'Contador Stateful',
-            onPressed: () => navigationService.navigatorTo('stateful'),
+            onPressed: () =>
+                locator<NavigationService>().navigatorTo('stateful'),
             color: Colors.black,
           ),
-          SizedBox(width: 10),
+          const SizedBox(width: 10),
           CustomFlatButton(
             text: 'Contador Provider',
-            onPressed: () => navigationService.navigatorTo('provider'),
+            onPressed: () =>
+                locator<NavigationService>().navigatorTo('provider'),
             color: Colors.black,
           ),
-          SizedBox(width: 10),
+          const SizedBox(width: 10),
           CustomFlatButton(
             text: 'Otra Pagina',
-            onPressed: () => navigationService.navigatorTo('abc123'),
+            onPressed: () => locator<NavigationService>().navigatorTo('abc123'),
             color: Colors.black,
           )
         ],
