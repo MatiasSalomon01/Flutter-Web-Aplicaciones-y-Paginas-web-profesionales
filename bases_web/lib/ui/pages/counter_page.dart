8c00c4e5401431d2b698c42d050908dd9a4ede88
MatiasSalomon01 <matisalomond@gmail.com>
2023-05-13 21:01:03 -0400

    Re-utilización de Widgets

--- bases_web/lib/ui/pages/counter_page.dart
+++ bases_web/lib/ui/pages/counter_page.dart
@@ -1,3 +1,4 @@
+import 'package:bases_web/ui/shared/custom_flat_button.dart';
 import 'package:flutter/material.dart';
 
 class CounterPage extends StatefulWidget {
@@ -17,15 +18,21 @@ class _CounterPageState extends State<CounterPage> {
         children: [
           Text(
             'Contador: $counter',
-            style: TextStyle(fontSize: 80, fontWeight: FontWeight.bold),
+            style: const TextStyle(fontSize: 80, fontWeight: FontWeight.bold),
           ),
-          TextButton(
-              onPressed: () {
-                setState(() {
-                  counter++;
-                });
-              },
-              child: const Text('Incrementar'))
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CustomFlatButton(
+                text: 'Incrementar',
+                onPressed: () => setState(() => counter++),
+              ),
+              CustomFlatButton(
+                text: 'Decrementar',
+                onPressed: () => setState(() => counter--),
+              ),
+            ],
+          )
         ],
       ),
     );
