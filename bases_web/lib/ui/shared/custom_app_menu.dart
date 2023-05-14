e65bfcaeb08e280a568e4fce674c63ca4491aa1b
MatiasSalomon01 <matisalomond@gmail.com>
2023-05-13 21:21:30 -0400

    Navegación tradicional en Flutter

--- bases_web/lib/ui/shared/custom_app_menu.dart
+++ bases_web/lib/ui/shared/custom_app_menu.dart
@@ -13,19 +13,19 @@ class CustomAppMenu extends StatelessWidget {
         children: [
           CustomFlatButton(
             text: 'Contador Stateful',
-            onPressed: () {},
+            onPressed: () => Navigator.pushNamed(context, 'stateful'),
             color: Colors.black,
           ),
           SizedBox(width: 10),
           CustomFlatButton(
             text: 'Contador Provider',
-            onPressed: () {},
+            onPressed: () => Navigator.pushNamed(context, 'provider'),
             color: Colors.black,
           ),
           SizedBox(width: 10),
           CustomFlatButton(
             text: 'Otra Pagina',
-            onPressed: () {},
+            onPressed: () => Navigator.pushNamed(context, 'abs123'),
             color: Colors.black,
           )
         ],
