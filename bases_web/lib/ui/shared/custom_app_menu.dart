47682cf62ccac7acb890daa5823d3b5c553327a2
MatiasSalomon01 <matisalomond@gmail.com>
2023-05-13 23:08:57 -0400

    Menú Responsivo

--- bases_web/lib/ui/shared/custom_app_menu.dart
+++ bases_web/lib/ui/shared/custom_app_menu.dart
@@ -7,11 +7,64 @@ class CustomAppMenu extends StatelessWidget {
   const CustomAppMenu({super.key});
 
   @override
+  Widget build(BuildContext context) => LayoutBuilder(
+        builder: (_, BoxConstraints constraints) {
+          return (constraints.maxWidth > 520)
+              ? const _TabletDesktopMenu()
+              : const _MobileMenu();
+        },
+      );
+}
+
+class _TabletDesktopMenu extends StatelessWidget {
+  const _TabletDesktopMenu({
+    super.key,
+  });
+
+  @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
       width: double.infinity,
       child: Row(
+        children: [
+          CustomFlatButton(
+            text: 'Contador Stateful',
+            onPressed: () =>
+                locator<NavigationService>().navigatorTo('stateful'),
+            color: Colors.black,
+          ),
+          const SizedBox(width: 10),
+          CustomFlatButton(
+            text: 'Contador Provider',
+            onPressed: () =>
+                locator<NavigationService>().navigatorTo('provider'),
+            color: Colors.black,
+          ),
+          const SizedBox(width: 10),
+          CustomFlatButton(
+            text: 'Otra Pagina',
+            onPressed: () => locator<NavigationService>().navigatorTo('abc123'),
+            color: Colors.black,
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class _MobileMenu extends StatelessWidget {
+  const _MobileMenu({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      width: double.infinity,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           CustomFlatButton(
             text: 'Contador Stateful',
