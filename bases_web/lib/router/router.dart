bc5abe3d359353b22da00840dff67d67a065b7dd
MatiasSalomon01 <matisalomond@gmail.com>
2023-05-15 18:55:33 -0400

    404 Page

--- bases_web/lib/router/router.dart
+++ bases_web/lib/router/router.dart
@@ -1,17 +1,38 @@
+import 'package:bases_web/ui/pages/page_404.dart';
+import 'package:bases_web/ui/views/counter_provider_view.dart';
 import 'package:bases_web/ui/views/counter_view.dart';
+import 'package:bases_web/ui/views/view_404.dart';
 import 'package:fluro/fluro.dart';
 
 class Flurorouter {
   static final FluroRouter router = FluroRouter();
 
   static void configureRoutes() {
-    router.define('/', handler: _counterHandler);
+    router.define('/',
+        handler: _counterHandler, transitionType: TransitionType.none);
+    router.define('/stateful',
+        handler: _counterHandler, transitionType: TransitionType.none);
+    router.define('/provider',
+        handler: _providerHandler, transitionType: TransitionType.none);
+    router.notFoundHandler = _pageNotFoundHandler;
   }
 
   //Handlers
   static final Handler _counterHandler = Handler(
     handlerFunc: (context, parameters) {
       return const CounterView();
+    },
+  );
+
+  static final Handler _providerHandler = Handler(
+    handlerFunc: (context, parameters) {
+      return const CounterProviderView();
+    },
+  );
+
+  static final Handler _pageNotFoundHandler = Handler(
+    handlerFunc: (context, parameters) {
+      return const View404();
     },
   );
 }
