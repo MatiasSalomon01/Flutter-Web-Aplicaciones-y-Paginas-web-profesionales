5e0c9e76a53ac3b1d3742e4eaff0f52ceb5b0f1f
MatiasSalomon01 <matisalomond@gmail.com>
2023-05-15 20:52:40 -0400

    falta completar video

--- scrollable_landing_page/lib/ui/shared/custom_app_menu.dart
+++ scrollable_landing_page/lib/ui/shared/custom_app_menu.dart
@@ -1,15 +1,27 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class CustomAppMenu extends StatelessWidget {
+class CustomAppMenu extends StatefulWidget {
   const CustomAppMenu({super.key});
 
+  @override
+  State<CustomAppMenu> createState() => _CustomAppMenuState();
+}
+
+class _CustomAppMenuState extends State<CustomAppMenu> {
+  bool isOpen = false;
   @override
   Widget build(BuildContext context) {
     return MouseRegion(
       cursor: SystemMouseCursors.click,
       child: GestureDetector(
-        onTap: () => print('click'),
+        onTap: () {
+          setState(() {
+            isOpen = !isOpen;
+          });
+        },
         child: Container(
           padding: const EdgeInsets.symmetric(horizontal: 10),
           width: 150,
@@ -17,12 +29,17 @@ class CustomAppMenu extends StatelessWidget {
           color: Colors.black,
           child: Row(
             children: [
+              AnimatedContainer(
+                duration: const Duration(milliseconds: 200),
+                curve: Curves.easeInOut,
+                width: isOpen ? 50 : 0,
+              ),
               Text(
                 'Menu',
                 style: GoogleFonts.roboto(color: Colors.white, fontSize: 18),
               ),
               const Spacer(),
-              const Icon(Icons.menu, color: Colors.white),
+              // AnimatedIcon(icon: AnimatedIcons.close_menu, progress: , color: Colors.white,)
             ],
           ),
         ),
