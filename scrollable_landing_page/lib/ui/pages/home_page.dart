b25c31c52b461b2b26f8e75f816e490631dfd1aa
MatiasSalomon01 <matisalomond@gmail.com>
2023-05-15 20:20:51 -0400

    Menú superior - Botón y Texto

--- scrollable_landing_page/lib/ui/pages/home_page.dart
+++ scrollable_landing_page/lib/ui/pages/home_page.dart
@@ -5,6 +5,8 @@ import 'package:scrollable_landing_page/ui/views/home_view.dart';
 import 'package:scrollable_landing_page/ui/views/location_view.dart';
 import 'package:scrollable_landing_page/ui/views/pricing_view.dart';
 
+import '../shared/custom_app_menu.dart';
+
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
@@ -14,17 +16,9 @@ class HomePage extends StatelessWidget {
         body: Container(
       decoration: _buildBoxDecoration(),
       child: Stack(
-        children: [
-          const _HomeBody(),
-          Positioned(
-            top: 20,
-            right: 20,
-            child: Container(
-              width: 150,
-              height: 50,
-              color: Colors.black,
-            ),
-          )
+        children: const [
+          _HomeBody(),
+          Positioned(top: 20, right: 20, child: CustomAppMenu())
         ],
       ),
     ));
@@ -32,10 +26,7 @@ class HomePage extends StatelessWidget {
 
   BoxDecoration _buildBoxDecoration() => const BoxDecoration(
         gradient: LinearGradient(
-          colors: [
-            Colors.pink,
-            Colors.deepPurpleAccent,
-          ],
+          colors: [Colors.pink, Colors.deepPurpleAccent],
           begin: Alignment.topCenter,
           end: Alignment.bottomCenter,
           stops: [0.5, 0.5],
---
+++ scrollable_landing_page/lib/ui/shared/custom_app_menu.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class CustomAppMenu extends StatelessWidget {
+  const CustomAppMenu({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MouseRegion(
+      cursor: SystemMouseCursors.click,
+      child: GestureDetector(
+        onTap: () => print('click'),
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          width: 150,
+          height: 50,
+          color: Colors.black,
+          child: Row(
+            children: [
+              Text(
+                'Menu',
+                style: GoogleFonts.roboto(color: Colors.white, fontSize: 18),
+              ),
+              const Spacer(),
+              const Icon(Icons.menu, color: Colors.white),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
