3fcb02bfe14ed95bad3673be4ec0d0833d1dc235
=Matias Salomon <matias.salomon@clt.com.py>
2023-05-19 16:02:16 -0400

    Cambiar el URL al hacer click en un enlace

--- scrollable_landing_page/lib/provider/page_provider.dart
+++ scrollable_landing_page/lib/provider/page_provider.dart
@@ -1,9 +1,15 @@
+// import 'dart:html' as html;
 import 'package:flutter/material.dart';
+import 'package:universal_html/html.dart' as html;
 
 class PageProvider extends ChangeNotifier {
   PageController scrollController = PageController();
 
+  List<String> _pages = ['home', 'about', 'pricing', 'contact', 'location'];
+
   goTo(int index) {
+    html.window.history.pushState(null, 'none', '#/${_pages[index]}');
+
     scrollController.animateToPage(
       index,
       duration: const Duration(milliseconds: 300),
