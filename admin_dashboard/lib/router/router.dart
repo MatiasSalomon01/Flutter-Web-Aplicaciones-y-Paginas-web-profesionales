27d32746b70329ed3a3c50c13f2ab6e1835734ca
=Matias Salomon <matias.salomon@clt.com.py>
2023-05-21 18:53:11 -0400

    No Page Found - Route

--- admin_dashboard/lib/router/router.dart
+++ admin_dashboard/lib/router/router.dart
@@ -1,4 +1,5 @@
 import 'package:admin_dashboard/router/admin_handlers.dart';
+import 'package:admin_dashboard/router/no_page_found_handlers.dart';
 import 'package:fluro/fluro.dart';
 
 class Flurorouter {
@@ -18,5 +19,7 @@ class Flurorouter {
     router.define(rootRoute, handler: AdminHandlers.login);
     router.define(loginRoute, handler: AdminHandlers.login);
     // router.define(registerRoute, handler: );
+
+    router.notFoundHandler = NotPageFoundHandlers.noPageFound;
   }
 }
