583eb13840d102c2d07f7f1545d102612db5ec8f
=Matias Salomon <matias.salomon@clt.com.py>
2023-05-21 19:05:13 -0400

    Diseño del AuthLayout

--- admin_dashboard/lib/ui/layouts/auth_layout.dart
+++ admin_dashboard/lib/ui/layouts/auth_layout.dart
@@ -6,10 +6,42 @@ class AuthLayout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Center(
-          child: Text('Auth Layout'),
-        ),
+      body: ListView(
+        children: [
+          //Desktop
+          _DesktopBody()
+          //Mobile
+
+          //Links
+        ],
+      ),
+    );
+  }
+}
+
+class _DesktopBody extends StatelessWidget {
+  const _DesktopBody({super.key});
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    return Container(
+      width: size.width,
+      height: size.height,
+      color: Colors.red,
+      child: Row(
+        children: [
+          Expanded(
+            child: Container(
+              color: Colors.blue,
+            ),
+          ),
+          Container(
+            width: 600,
+            height: double.infinity,
+            color: Colors.black,
+          )
+        ],
       ),
     );
   }
