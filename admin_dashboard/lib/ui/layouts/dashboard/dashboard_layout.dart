98adef31b02eaace23037e2173082bb2e0a687c0
=Matias Salomon <matias.salomon@clt.com.py>
2023-05-22 16:39:51 -0400

    Continuación de proyecto - AdminDashboard

--- admin_dashboard/lib/ui/layouts/dashboard/dashboard_layout.dart
+++ admin_dashboard/lib/ui/layouts/dashboard/dashboard_layout.dart
@@ -7,6 +7,7 @@ class DashboardLayout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0xffedf1f2),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
