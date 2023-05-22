25b9af36e4cf37ae4b6236dfc86090fb27e8a56c
=Matias Salomon <matias.salomon@clt.com.py>
2023-05-22 12:48:53 -0400

    LocalStorage

--- admin_dashboard/lib/providers/auth_provider.dart
+++ admin_dashboard/lib/providers/auth_provider.dart
@@ -1,14 +1,12 @@
+import 'package:admin_dashboard/services/local_storage.dart';
 import 'package:flutter/material.dart';
 
 class AuthProvider extends ChangeNotifier {
   String? _token;
 
   login(String email, String password) {
-    //TODO: Peticion HTTP
     _token = 'asdaosdoasdokasodkasodkasodkasodkasd';
-    print('alamcenar JWT: $_token');
-
-    //TODO: Navegat el Dashboard
+    LocalStorage.prefs.setString('token', _token!);
     notifyListeners();
   }
 }
