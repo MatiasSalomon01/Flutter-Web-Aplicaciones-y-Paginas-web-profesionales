ede5d1089a4ae411447699137d66fb3fab5aa0b9
=Matias Salomon <matias.salomon@clt.com.py>
2023-05-22 12:55:49 -0400

    NavigationService

--- admin_dashboard/lib/main.dart
+++ admin_dashboard/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:admin_dashboard/providers/auth_provider.dart';
 import 'package:admin_dashboard/router/router.dart';
 import 'package:admin_dashboard/services/local_storage.dart';
+import 'package:admin_dashboard/services/navigation_service.dart';
 import 'package:admin_dashboard/ui/layouts/auth/auth_layout.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -37,6 +38,7 @@ class MyApp extends StatelessWidget {
       title: 'Admin Dashboard',
       initialRoute: Flurorouter.rootRoute,
       onGenerateRoute: Flurorouter.router.generator,
+      navigatorKey: NavigationService.navigatorKey,
       builder: (context, child) {
         return AuthLayout(child: child!);
       },
