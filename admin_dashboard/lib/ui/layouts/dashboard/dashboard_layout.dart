b2fa4bbf7a1c16ae95894c9fbff5618c1909acc4
=Matias Salomon <matias.salomon@clt.com.py>
2023-05-23 16:48:50 -0400

    Animar menú lateral

--- admin_dashboard/lib/ui/layouts/dashboard/dashboard_layout.dart
+++ admin_dashboard/lib/ui/layouts/dashboard/dashboard_layout.dart
@@ -1,10 +1,27 @@
+import 'package:admin_dashboard/providers/sidemenu_provider.dart';
 import 'package:admin_dashboard/ui/shared/navbar.dart';
 import 'package:admin_dashboard/ui/shared/sidebar.dart';
 import 'package:flutter/material.dart';
 
-class DashboardLayout extends StatelessWidget {
+class DashboardLayout extends StatefulWidget {
   final Widget child;
   const DashboardLayout({super.key, required this.child});
+
+  @override
+  State<DashboardLayout> createState() => _DashboardLayoutState();
+}
+
+class _DashboardLayoutState extends State<DashboardLayout>
+    with SingleTickerProviderStateMixin {
+  @override
+  void initState() {
+    super.initState();
+
+    SideMenuProvider.menuController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 300),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,13 +38,24 @@ class DashboardLayout extends StatelessWidget {
                 child: Column(
                   children: [
                     const NavBar(),
-                    Expanded(child: child),
+                    Expanded(child: widget.child),
                   ],
                 ),
               ),
             ],
           ),
-          if (size.width < 700) const SideBar(),
+          if (size.width < 700)
+            AnimatedBuilder(
+              animation: SideMenuProvider.menuController,
+              builder: (context, child) => Stack(
+                children: [
+                  Transform.translate(
+                    offset: Offset(SideMenuProvider.movement.value, 0),
+                    child: const SideBar(),
+                  ),
+                ],
+              ),
+            )
         ],
       ),
     );
