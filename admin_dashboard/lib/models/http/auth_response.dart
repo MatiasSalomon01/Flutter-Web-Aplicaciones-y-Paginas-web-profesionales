8c973de3bd899d20a0e5077cc720223c045f2404
=Matias Salomon <matias.salomon@clt.com.py>
2023-05-25 14:29:50 -0400

    Mapear categorías

--- admin_dashboard/lib/models/http/auth_response.dart
+++ admin_dashboard/lib/models/http/auth_response.dart
@@ -1,7 +1,3 @@
-// To parse this JSON data, do
-//
-//     final authResponse = authResponseFromJson(jsonString);
-
 import 'dart:convert';
 
 class AuthResponse {
--- admin_dashboard/lib/providers/categories_provider.dart
+++ admin_dashboard/lib/providers/categories_provider.dart
@@ -1,11 +1,15 @@
 import 'package:admin_dashboard/api/CafeApi.dart';
+import 'package:admin_dashboard/models/http/categories_response.dart';
+import 'package:admin_dashboard/models/http/category.dart';
 import 'package:flutter/material.dart';
 
 class CategoriesProvider extends ChangeNotifier {
-  List categorias = [];
+  List<Categoria> categorias = [];
 
   getCategories() async {
     final res = await CafeApi.httpGet('/categorias');
+    final categoriesResponse = CategoriesResponse.fromJson(res);
+    categorias = [...categoriesResponse.categorias];
     notifyListeners();
   }
 }
