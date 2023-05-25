d82cbb6a3e84a6f9c226c86dc527b0e8e4b925ed
=Matias Salomon <matias.salomon@clt.com.py>
2023-05-25 16:03:43 -0400

    Contenido del modal de categoría

--- admin_dashboard/lib/ui/buttons/custom_outlined_button.dart
+++ admin_dashboard/lib/ui/buttons/custom_outlined_button.dart
@@ -31,10 +31,10 @@ class CustomOutlinedButton extends StatelessWidget {
       ),
       onPressed: () => onPressed(),
       child: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
         child: Text(
           text,
-          style: const TextStyle(fontSize: 16),
+          style: TextStyle(fontSize: 16, color: color),
         ),
       ),
     );
--- admin_dashboard/lib/ui/modals/category_modal.dart
+++ admin_dashboard/lib/ui/modals/category_modal.dart
@@ -1,4 +1,7 @@
 import 'package:admin_dashboard/models/http/category.dart';
+import 'package:admin_dashboard/ui/buttons/custom_outlined_button.dart';
+import 'package:admin_dashboard/ui/inputs/custom_inputs.dart';
+import 'package:admin_dashboard/ui/labels/custom_labels.dart';
 import 'package:flutter/material.dart';
 
 class CategoryModal extends StatefulWidget {
@@ -23,8 +26,54 @@ class _CategoryModalState extends State<CategoryModal> {
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: const EdgeInsets.all(20),
       height: 500,
       decoration: buildBoxDecoration(),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                widget.categoria?.nombre ?? "Nueva Categoria",
+                style: CustomLabels.h1.copyWith(
+                  color: Colors.white,
+                ),
+              ),
+              IconButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                icon: const Icon(
+                  Icons.close,
+                  color: Colors.white,
+                ),
+              )
+            ],
+          ),
+          Divider(color: Colors.white.withOpacity(0.3)),
+          const SizedBox(height: 20),
+          TextFormField(
+            initialValue: widget.categoria?.nombre ?? '',
+            onChanged: (value) => nombre = value,
+            decoration: CustomInputs.loginInputDecoration(
+              hint: 'Nombre de la categoria',
+              label: 'Categoria',
+              icon: Icons.new_releases_outlined,
+            ),
+            style: const TextStyle(color: Colors.white),
+          ),
+          Container(
+            margin: const EdgeInsets.only(top: 30),
+            alignment: Alignment.center,
+            child: CustomOutlinedButton(
+              onPressed: () async {},
+              text: 'Guardar',
+              color: Colors.white,
+            ),
+          )
+        ],
+      ),
     );
   }
 
