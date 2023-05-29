9e5b0c67d8e14a6c4a09ce8c9d0ada5fabed95f0
=Matias Salomon <matias.salomon@clt.com.py>
2023-05-29 12:12:06 -0400

    Reduciendo la cantidad de importaciones

--- admin_dashboard/lib/main.dart
+++ admin_dashboard/lib/main.dart
@@ -1,18 +1,10 @@
 import 'package:admin_dashboard/api/CafeApi.dart';
-import 'package:admin_dashboard/providers/auth_provider.dart';
-import 'package:admin_dashboard/providers/categories_provider.dart';
-import 'package:admin_dashboard/providers/sidemenu_provider.dart';
-import 'package:admin_dashboard/providers/user_form_provider.dart';
-import 'package:admin_dashboard/providers/users_provider.dart';
 import 'package:admin_dashboard/router/router.dart';
-import 'package:admin_dashboard/services/local_storage.dart';
-import 'package:admin_dashboard/services/navigation_service.dart';
-import 'package:admin_dashboard/services/notifications_service.dart';
-import 'package:admin_dashboard/ui/layouts/auth/auth_layout.dart';
-import 'package:admin_dashboard/ui/layouts/dashboard/dashboard_layout.dart';
-import 'package:admin_dashboard/ui/layouts/splash/splash_layout.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'providers/providers.dart';
+import 'services/services.dart';
+import 'ui/ui.dart';
 
 void main() async {
   await LocalStorage.configurePrefs();
